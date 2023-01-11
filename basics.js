cf7fdfa02f7f483f164ae715bd2950b22cc2bf19
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2023-01-12 02:30:42 +0530

    add basic.js

--- basics.js
+++ basics.js
@@ -1,8 +1,21 @@
+// Description: Basic example on using the nodejs stream 
+// file: basics.js
+
 const fs = require('fs');
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
+
 
 // create a readable stream to read the CSV file
-const readStream = fs.createReadStream('large_file.csv');
+const filePath = 'data/large_file.csv';
+const readStream = fs.createReadStream(filePath);
+
+// read the size of the file
+const stats = fs.statSync(filePath);
+const fileSizeInBytes = stats.size;
+console.log(`File Path: ${filePath}`);
+console.log(`File size: ${fileSizeInBytes} bytes`);
+
+let totalRows = 0;
 
 // create a CSV parser
 const parser = parse({
@@ -11,13 +24,15 @@ const parser = parse({
 
 // handle the data event
 parser.on('data', (data) => {
-    console.log(data);
+    // console.log(data);
+    totalRows += 1;
     // do something with the data, e.g. insert into a database
 });
 
 // handle the end event
 parser.on('end', () => {
     console.log('Finished reading CSV file');
+    console.log(`Total rows: ${totalRows}`);
 });
 
 // handle any errors
